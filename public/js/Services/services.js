5147f56a3887cd6fa281a9a06ec9bdd647876e53
= <jrodrigomg@hotmail.com>
2015-12-27 19:49:02 -0600

    ip haproxy in socket io client

--- public/js/Services/services.js
+++ public/js/Services/services.js
@@ -1,7 +1,7 @@
 angular.module('services',[])
 
 .factory('$socket',function(socketFactory){
-	var socket = io.connect("http://192.168.122.121:3000",{transports: ["websocket"]});
+	var socket = io.connect("http://192.168.2.30:3000",{transports: ["websocket"]});
 	mySocket = socketFactory({
 		ioSocket: socket
 	});
