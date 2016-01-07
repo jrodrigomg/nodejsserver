bb5d737d114ec55e2311a666f0ed64b1d652e127
= <jrodrigomg@hotmail.com>
2016-01-07 12:44:27 -0600

    reconnection attemps and delay

--- public/js/Services/services.js
+++ public/js/Services/services.js
@@ -1,7 +1,7 @@
 angular.module('services',[])
 
 .factory('$socket',function(socketFactory){
-	var socket = io.connect("192.168.2.30",{'force new connection':true, 'max reconnection attemps': 10,transports: ["websocket"],'reconnection':true,'reconnection delay':5000});
+	var socket = io.connect("192.168.2.30",{'force new connection':true, 'max reconnection attemps': 3,transports: ["websocket"],'reconnection':true,'reconnection delay':1000});
 	mySocket = socketFactory({
 		ioSocket: socket
 	});
